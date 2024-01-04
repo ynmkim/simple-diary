9044b329ec75f1e8ddb5acca9fea9d50730e4f19
김윤미 <younmeet@gmail.com>
2024-01-04 14:25:41 +0900

    feat: api 호출, dummy 데이터 가공

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './styles/App.css';
 import './styles/global.css';
 import DiaryEdit from './DiaryEdit';
@@ -7,6 +7,29 @@ import DiaryList from './DiaryList';
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    );
+    const body = await response.json();
+
+    const initData = body.slice(0, 20).map((item) => {
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        createdAt: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const createdAt = new Date().getTime();
